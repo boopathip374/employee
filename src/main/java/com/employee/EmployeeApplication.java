8fa2cc3e6718256503a112ddaf3a4558a9691fba
Boopathi <boopathi@Boopathis-MacBook-Air.local>
2022-07-11 14:46:25 +0530

    Code fix

--- src/main/java/com/employee/EmployeeApplication.java
+++ src/main/java/com/employee/EmployeeApplication.java
@@ -4,15 +4,38 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.employee.repository")
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
+	
+//	 @Bean
+//	 public MongoDbFactory mongoDbFactory() throws Exception {
+//	        return new SimpleMongoDbFactory(new Mongo(), "employee");
+//	    }
+//
+//	    @Bean
+//	    public MongoTemplate mongoTemplate() throws Exception {
+//
+//	        //remove _class
+//	        MappingMongoConverter converter = new MappingMongoConverter(mongoDbFactory(), new MongoMappingContext());
+//	        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+//
+//	        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory(), converter);
+//
+//	        return mongoTemplate;
+//
+//	    }
 	
 }
