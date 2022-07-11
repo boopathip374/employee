de8560763a9ebbe11e87329505c538005d22edf1
Boopathi <boopathi@Boopathis-MacBook-Air.local>
2022-07-11 11:34:22 +0530

    Code fix

--- src/main/java/com/employee/repository/EmployeeRepo.java
+++ src/main/java/com/employee/repository/EmployeeRepo.java
@@ -1,9 +1,11 @@
 package com.employee.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.employee.domain.EmployeeEntity;
 
+@Repository
 public interface EmployeeRepo extends MongoRepository<EmployeeEntity, String>{
 
 }
