565453d77c37596864b942331e5f89cb2e92d9dd
Boopathi <boopathi@Boopathis-MacBook-Air.local>
2022-07-11 13:04:36 +0530

    Code fix

--- src/main/java/com/employee/EmployeeApplication.java
+++ src/main/java/com/employee/EmployeeApplication.java
@@ -4,25 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import com.mongodb.client.MongoClient;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+@EnableMongoRepositories(basePackages = "com.employee.repository")
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
-//	@Bean
-//	public MongoClient mongoClient() {
-//	    return new MongoClient("localhost", 27017);
-//	}
-
-	@Bean
-	public MongoTemplate mongoTemplate(MongoClient mongoClient) throws Exception {
-	    return new MongoTemplate(mongoClient, "employee");
-	}
+	
 }
