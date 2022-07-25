9e431c1a2a2590ab7e1d6ec458477b3d8b46759b
Boopathi <boopathi@Boopathis-MacBook-Air.local>
2022-07-25 21:22:57 +0530

    testing

--- src/main/java/com/employee/controller/Controller.java
+++ src/main/java/com/employee/controller/Controller.java
@@ -36,43 +36,43 @@ public class Controller {
 	
 	
 	
-//	@PostMapping("/create")
-//	public ResponseEntity<?> createEmployee(@RequestBody RequestEmployeeService requestEmployeeService)
-//			throws ApiException {
-//		Response response = null;
-//		try {
-//			ResponseEntity<ErrorResponse> responseEntity = employeeServiceValidation
-//					.validateEmployeeRequest(requestEmployeeService);
-//			if (null != responseEntity) {
-//				return responseEntity;
-//			} else {
-//				response = employeeService.create(requestEmployeeService);
-//				return new ResponseEntity<Response>(response, HttpStatus.CREATED);
-//			}
-//		} catch (Exception ex) {
-//			throw new ApiException(EmployeeConstants.ERROR_TYPE, EmployeeConstants.ERROR_CODE_500, ex.getMessage());
-//		}
-//	}
-	 
-	 
-	
-	@PutMapping("/update")
-	public ResponseEntity<?> updateEmployee(@RequestBody RequestEmployeeService requestEmployeeService)
+	@PostMapping("/create")
+	public ResponseEntity<?> createEmployee(@RequestBody RequestEmployeeService requestEmployeeService)
 			throws ApiException {
 		Response response = null;
 		try {
 			ResponseEntity<ErrorResponse> responseEntity = employeeServiceValidation
-					.validateUpdateEmployeeRequest(requestEmployeeService);
+					.validateEmployeeRequest(requestEmployeeService);
 			if (null != responseEntity) {
 				return responseEntity;
 			} else {
-				response = employeeService.update(requestEmployeeService);
-				return new ResponseEntity<Response>(response, HttpStatus.OK);
+				response = employeeService.create(requestEmployeeService);
+				return new ResponseEntity<Response>(response, HttpStatus.CREATED);
 			}
 		} catch (Exception ex) {
 			throw new ApiException(EmployeeConstants.ERROR_TYPE, EmployeeConstants.ERROR_CODE_500, ex.getMessage());
 		}
 	}
+	 
+	 
+	
+//	@PutMapping("/update")
+//	public ResponseEntity<?> updateEmployee(@RequestBody RequestEmployeeService requestEmployeeService)
+//			throws ApiException {
+//		Response response = null;
+//		try {
+//			ResponseEntity<ErrorResponse> responseEntity = employeeServiceValidation
+//					.validateUpdateEmployeeRequest(requestEmployeeService);
+//			if (null != responseEntity) {
+//				return responseEntity;
+//			} else {
+//				response = employeeService.update(requestEmployeeService);
+//				return new ResponseEntity<Response>(response, HttpStatus.OK);
+//			}
+//		} catch (Exception ex) {
+//			throw new ApiException(EmployeeConstants.ERROR_TYPE, EmployeeConstants.ERROR_CODE_500, ex.getMessage());
+//		}
+//	}
 	 
 	
 	@GetMapping("/getemployee")
