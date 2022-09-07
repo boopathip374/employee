be00e1b69a935d71e5cab3a8e426694997d16536
Boopathi <boopathi@Boopathis-MacBook-Air.local>
2022-09-07 13:28:06 +0530

    IP chnage

--- src/main/java/com/employee/controller/Controller.java
+++ src/main/java/com/employee/controller/Controller.java
@@ -36,24 +36,24 @@ public class Controller {
 	
 	
 
-	@PostMapping("/create")
-	public ResponseEntity<?> createEmployee(@RequestBody RequestEmployeeService requestEmployeeService)
-			throws ApiException {
-		Response response = null;
-		try {
-			ResponseEntity<ErrorResponse> responseEntity = employeeServiceValidation
-					.validateEmployeeRequest(requestEmployeeService);
-			if (null != responseEntity) {
-				return responseEntity;
-			} else {
-				response = employeeService.create(requestEmployeeService);
-				return new ResponseEntity<Response>(response, HttpStatus.CREATED);
-			}
-		} catch (Exception ex) {
-			throw new ApiException(EmployeeConstants.ERROR_TYPE, EmployeeConstants.ERROR_CODE_500, ex.getMessage());
-		}
-	}
-	 
+//	@PostMapping("/create")
+//	public ResponseEntity<?> createEmployee(@RequestBody RequestEmployeeService requestEmployeeService)
+//			throws ApiException {
+//		Response response = null;
+//		try {
+//			ResponseEntity<ErrorResponse> responseEntity = employeeServiceValidation
+//					.validateEmployeeRequest(requestEmployeeService);
+//			if (null != responseEntity) {
+//				return responseEntity;
+//			} else {
+//				response = employeeService.create(requestEmployeeService);
+//				return new ResponseEntity<Response>(response, HttpStatus.CREATED);
+//			}
+//		} catch (Exception ex) {
+//			throw new ApiException(EmployeeConstants.ERROR_TYPE, EmployeeConstants.ERROR_CODE_500, ex.getMessage());
+//		}
+//	}
+//	 
 	 
 	
 	@PutMapping("/update")
